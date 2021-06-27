c8144497bde6f0044cf1f3b3a28533bd83809eff
Abhishek Kumar <abhik12295@gmail.com>
2021-06-27 12:01:19 -0500

    Update 25_running_time-complexity.py

--- 30 days/25_running_time-complexity.py
+++ 30 days/25_running_time-complexity.py
@@ -1,26 +1,11 @@
-# x=int(input())
-# for i in range(x):
-#     z=int(input())
-#     if z>1:
-#         for j in range(2,z):
-#             if(z%j)==0:
-#                 print("Not prime")
-#                 break
-#         else:
-#             print("Prime number")
-#             break
-#     else:
-#         print("Not prime")
-import math
-
 x=int(input())
 for i in range(x):
-    z=int(input())
-    if z<2:
+    num=int(input())
+    if num<2:
         print("Not prime")
-    elif z>1:
-        for j in range(2,int(z**0.5)+1):  #No need to check the repeated factors again
-            if z%j==0:
+    elif num>1:
+        for j in range(2,int(num**0.5)+1):
+            if num%j==0:
                 print("Not prime")
                 break
         else:
