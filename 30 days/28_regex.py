47f43c8695bae1b31a3793162df4cb10f3fbe19f
Abhishek Kumar <abhik12295@gmail.com>
2021-06-27 15:11:35 -0500

    Update 28_regex.py

--- 30 days/28_regex.py
+++ 30 days/28_regex.py
@@ -16,9 +16,31 @@ if __name__ == '__main__':
         if match:
             lst.append(firstName)
     y=sorted(lst)
-    print('\n'.join(lst))
-    # y = sorted(lst)
-    # print(*y, sep='\n')
+    print('\n'.join(y))
+    
+#witohout using regex
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    lst = []
+    for N_itr in range(N):
+        first_multiple_input = input().rstrip().split()
+
+        firstName = first_multiple_input[0]
+
+        emailID = first_multiple_input[1]
+        if '@gmail.com' in emailID:
+            lst.append(firstName)
+    y = sorted(lst)
+    print('\n'.join(y))
+        
 
 
 
