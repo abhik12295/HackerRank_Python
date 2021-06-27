92c463c7c3eeeb155c909b36a791486506bb3719
Abhishek Kumar <abhik12295@gmail.com>
2021-06-27 15:54:49 -0500

    Update 29_bitwise.py

--- 30 days/29_bitwise.py
+++ 30 days/29_bitwise.py
@@ -1,11 +1,44 @@
-x=int(input())
-for i in range(x):
-    nk=input().split(" ",2)
-    n=int(nk[0])
-    k=int(nk[1])
-    newlist = []
-    print(k-1 if ((k-1) | k) <= n else k-2)
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
 
+#
+# Complete the 'bitwiseAnd' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER N
+#  2. INTEGER K
+#
 
+def bitwiseAnd(N, K):
+    # Write your code here
+    res = 0
+    for i in range(K-2,N+1):
+        for j in range(i+1,N+1):
+            and_store = i&j
+            if and_store < K and and_store > res:
+                res = and_store
+    return res
 
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        first_multiple_input = input().rstrip().split()
+
+        count = int(first_multiple_input[0])
+
+        lim = int(first_multiple_input[1])
+
+        res = bitwiseAnd(count, lim)
+
+        fptr.write(str(res) + '\n')
+
+    fptr.close()
