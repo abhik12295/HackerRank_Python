5176d65e4c5be86901c68a10088448756335d890
Abhishek Kumar <abhik12295@gmail.com>
2021-06-27 11:22:55 -0500

    Update 23_BST_Traversal.py

--- 30 days/23_BST_Traversal.py
+++ 30 days/23_BST_Traversal.py
@@ -1,39 +1,40 @@
 import sys
 
-
 class Node:
-    def __init__(self, data):
-        self.right = self.left = None
+    def __init__(self,data):
+        self.right=self.left=None
         self.data = data
-
-
 class Solution:
-    def insert(self, root, data):
-        if root == None:
+    def insert(self,root,data):
+        if root==None:
             return Node(data)
         else:
-            if data <= root.data:
-                cur = self.insert(root.left, data)
-                root.left = cur
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
             else:
-                cur = self.insert(root.right, data)
-                root.right = cur
+                cur=self.insert(root.right,data)
+                root.right=cur
         return root
 
-    def levelOrder(self, root):
-        q=[root]
-        while q:
-            n=q.pop(0)
-            print(n.data)
-            if n.left:
-                q.append(n.left)
-            if n.right:
-                q.append(n.right)
+    def levelOrder(self,root):
+        #Write your code here
+        if root is None:
+            return
+        q =[]
+        q.append(root)
+        while len(q) > 0:
+            node = q.pop(0)
+            print(node.data, end = ' ')
+            if node.left is not None:
+                q.append(node.left)
+            if node.right is not None:
+                q.append(node.right)
 
-T = int(input())
-myTree = Solution()
-root = None
+T=int(input())
+myTree=Solution()
+root=None
 for i in range(T):
-    data = int(input())
-    root = myTree.insert(root, data)
+    data=int(input())
+    root=myTree.insert(root,data)
 myTree.levelOrder(root)
